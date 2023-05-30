b6be849a98793207bb71c3aa4078bf3eb1c4332e
Alexander Dolbik <alexandr.dolbik@gmail.com>
2023-05-30 21:01:34 +0200

    Init

--- prHandler.js
+++ prHandler.js
@@ -6,35 +6,6 @@ process.on('unhandledRejection', (err) => {
   console.error(err)
 })
 
-async function handlePullRequest(eventPayload) {
-  console.log('Start "handlePullRequest"');
-
-  // Extract relevant information from the event payload
-  const { pull_request: pr, repository } = eventPayload;
-  const { number, title, body, head, base } = pr;
-  const { owner, repo } = repository;
-
-  // Perform actions based on the PR information
-  console.log(`New PR created: #${number}`);
-  console.log(`Title: ${title}`);
-  console.log(`Description: ${body}`);
-  console.log(`Branch: ${head.ref}`);
-  console.log(`Base branch: ${base.ref}`);
-  console.log(`Repository: ${owner}/${repo}`);
-
-  // Additional logic for interacting with ChatGPT or performing other actions
-
-  // Example: Comment on the PR
-  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-  const comment = "Hello! This is a comment from the PR handler.";
-  await octokit.issues.createComment({
-    owner,
-    repo,
-    issue_number: number,
-    body: comment,
-  });
-}
-
 const main = async () => {
   try {
     /**
@@ -157,5 +128,3 @@ const main = async () => {
 
 // Call the main function to run the action
 main();
-
-module.exports = handlePullRequest;
